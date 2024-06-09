4eca198b4d82126ef03d2a54f4db97fe9195534d
Ninad Bhole <ninad.bhole@plansource.com>
2024-06-09 13:04:45 +0530

    Dynamic CSS styling for TabButtonns

--- src/App.jsx
+++ src/App.jsx
@@ -31,10 +31,10 @@ function App() {
         </section>
         <section id = "examples">
           <menu>
-          <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-          <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-          <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-          <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          <TabButton isSelected= {selectedTopic === 'components' } onSelect={() => handleSelect('components')}>Components</TabButton>
+          <TabButton isSelected= {selectedTopic === 'jsx' } onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+          <TabButton isSelected= {selectedTopic === 'props' } onSelect={() => handleSelect('props')}>Props</TabButton>
+          <TabButton isSelected= {selectedTopic === 'state' } onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
           {
             selectedTopic ? <div id="tab-content">
